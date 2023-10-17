cbed483aba55c919275172a7c61b2f65509a1e30
iago <gmziago8@gmail.com>
2023-10-17 08:13:44 -0300

    primeiro commit

---
+++ aula1.py
@@ -0,0 +1,16 @@
+"""
+print('Olá Mundo!')
+"""
+
+# Isso é um comentário
+'''
+print('Linha1')
+print('Linha2')
+'''
+
+print('Meu nome é Fabio')
+
+print('Linha3')
+print('Linha4')  #Isso é um comentário
+print('Linha5')
+print('Linha6')  #Outro comentário
---
+++ aula3.py
@@ -0,0 +1,15 @@
+#print('alguma coisa')
+#print("Aspas duplas")
+#print(123456)
+
+"""
+str - string
+"""
+
+print("Essa é uma 'string' (str).")
+print('Essa é uma "string" (str).')
+print("Esse é meu \"texto\" (str).")
+print('Esse é meu \n (str).')
+print(r'Esse é meu \n (str).')
+
+#''  "" () {}  []
